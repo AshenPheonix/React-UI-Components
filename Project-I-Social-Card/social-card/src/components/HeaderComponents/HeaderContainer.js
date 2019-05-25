f61ac8d80455e87d020629b12b73969f9191a5a3
Brandon Porter <brendlorend@gmail.com>
2019-05-25 16:22:23 -0700

    MVP 1: Monday Done

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -5,26 +5,21 @@ import Title from './HeaderTitle';
 import Content from './HeaderContent';
 
 export default class HeaderContainer extends Component {
-    constructor(props) {
-        super(props)
-        if(!this.props.title)
-            this.props.title='Lambda School'
-        if(!this.props.handle)
-            this.props.handle='@LambdaSchool'
-        if(this.props.date)
-            this.props.date=new Date('26 Jan')
-        if(!this.props.src)
-            this.props.src='https://tk-assets.lambdaschool.com/1c1b7262-cf23-4a9f-90b6-da0d3c74a5c6_lambdacrest.png'
-        if(!this.props.alt)
-            this.props.alt='Lambda School Logo'
-    }
-    
     render() {
         return (
             <section className="head">
-                <Thumbnail/>
-                <Title/>
-                <Content/>
+                <Thumbnail 
+                    src={this.props.head.src}
+                    alt={this.props.head.alt}
+                />
+                <Title 
+                    title={this.props.head.title}
+                    handle={this.props.head.handle}
+                    date={this.props.head.date}
+                />
+                <Content
+                    copy={this.props.head.copy}
+                />
             </section>
         )
     }
